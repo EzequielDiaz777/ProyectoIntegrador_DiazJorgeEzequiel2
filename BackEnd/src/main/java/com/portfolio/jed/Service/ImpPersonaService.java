1c608a70085715546167c77e927e795c3c520634
EzequielDiaz777 <82990669+EzequielDiaz777@users.noreply.github.com>
2022-05-20 20:16:54 -0300

    Initial commit

---
+++ BackEnd/src/main/java/com/portfolio/jed/Service/ImpPersonaService.java
@@ -0,0 +1,38 @@
+package com.portfolio.jed.Service;
+
+import com.portfolio.jed.Entity.Persona;
+import com.portfolio.jed.Interface.IPersonaService;
+import com.portfolio.jed.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService {
+
+    @Autowired
+    IPersonaRepository ipersonaRepository;
+
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> personas = ipersonaRepository.findAll();
+        return personas;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+
+}
